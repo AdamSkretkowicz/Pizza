d1e86ec60e9dd51d8aa52bea9437cb8b1a35fd13
lap_Adam_Skretkowicz <adam.skretkowicz@gmail.com>
2020-01-25 16:59:56 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //pobranie informacji od uzytkownika o ilosci pizzy
+        System.out.println("Wpisz Ilosc pizzy do porownania: ");
+        Scanner wpiszIloscPizzy = new Scanner(System.in);
+        int iloscPizz = wpiszIloscPizzy.nextInt();
+
+        if (iloscPizz == 0 || iloscPizz < 0) {
+            System.out.println("Wpisales niepoprawną ilosc, koncze program");
+        } else if (iloscPizz > 0) {
+            System.out.println("Wpisales " + iloscPizz + " do porownania, podaj ich parametry: [rozmiar(cm),cena]");
+
+            Double[] tablicaObliczeniowa = new Double[iloscPizz];
+            for (int i = 1; i <= iloscPizz; i++) {
+                System.out.println("Podaj parametry dla " + i + " pizzy, rozmiar:");
+                Scanner wpiszRozmiar = new Scanner(System.in);
+                double rozmiarPizzy = wpiszRozmiar.nextInt();
+                System.out.println("podaj cene:");
+                Scanner wpiszCene = new Scanner(System.in);
+                double cenaPizzy = wpiszCene.nextInt();
+                System.out.println("Pizza nr " + i + " rozmiar: " + rozmiarPizzy + " cena: " + cenaPizzy);
+
+                double wspolczynnik = rozmiarPizzy / cenaPizzy;
+                System.out.println("wspolczynnik :" + wspolczynnik);
+                tablicaObliczeniowa[i - 1] = wspolczynnik;
+            }
+            double max = 0;
+            int max_index = 0;
+            for (int a = 0; a < tablicaObliczeniowa.length; a++) {
+                if (tablicaObliczeniowa[a] > max) {
+                    max = tablicaObliczeniowa[a];
+                    max_index = a + 1;
+                }
+            }
+            System.out.println("Wspolczynnik oplacalnosci : " + max + " posiada pizza nr : " + max_index);
+        }
+    }
+}
